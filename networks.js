c74c9c210416d9a8ba7d0d6fc43893848e64e1e2
Zaki Manian <zaki@manian.org>
2014-08-13 16:05:38 -0700

    Added definitions for the dogecoin networks to networks.js

--- networks.js
+++ networks.js
@@ -4,8 +4,8 @@ var hex = function(hex) {
   return new Buffer(hex, 'hex');
 };
 
-exports.livenet = {
-  name: 'livenet',
+exports.bitcoin.livenet = {
+  name: 'BitCoin livenet',
   magic: hex('f9beb4d9'),
   addressVersion: 0x00,
   privKeyVersion: 128,
@@ -33,10 +33,10 @@ exports.livenet = {
   defaultClientPort: 8333
 };
 
-exports.mainnet = exports.livenet;
+exports.bitcoin.mainnet = exports.bitcoin.livenet;
 
-exports.testnet = {
-  name: 'testnet',
+exports.bitcoin.testnet = {
+  name: 'Bitcoin testnet',
   magic: hex('0b110907'),
   addressVersion: 0x6f,
   privKeyVersion: 239,
@@ -59,3 +59,69 @@ exports.testnet = {
   ],
   defaultClientPort: 18333
 };
+
+exports.dogecoin.livenet = {
+  name: 'DogeCoin livenet',
+  addressVersion: 0x1E,
+  magic: hex('c0c0c0c0'),
+  genesisBlock: {
+    hash: hex('1a91e3dace36e2be3bf030a65679fe821aa1d6ef92e7c9902eb318182c355691'),
+    merkle_root: hex('5b2a3f53f605d62c53e62932dac6925e3d74afa5a4b459745c36d42d0ed26a69'),
+    height: 0,
+    nonce: 99943,
+    version: 1,
+    prev_hash: buffertools.fill(new Buffer(32), 0),
+    timestamp: 1386325540,
+    bits: 0x1e0ffff0
+  },
+  addressPubkey: 0x1E,
+  addressScript: 5,
+  hkeyPublicVersion: 0x0488b21e,
+  hkeyPrivateVersion: 0x0488ade4,
+  privKeyVersion: 128,
+  dnsSeeds: [
+    '95.85.29.144',
+    '162.243.113.110',
+    '146.185.181.114',
+    'doge.netcodepool.org',
+    'doge.cryptoculture.net',
+    'dogepool.pw',
+    '78.46.57.132'
+  ],
+  defaultClientPort: 22556
+
+};
+
+exports.dogecoin.testnet = {
+  name: 'DogeCoin testnet',
+  addressVersion: 0x6f,
+  magic: hex('fcc1b7dc'),
+  genesisBlock: {
+    hash: hex('43497FD7F826957108F4A30FD9CEC3AEBA79972084E90EAD01EA330900000000'),
+    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    height: 0,
+    nonce: 414098458,
+    version: 1,
+    prev_hash: buffertools.fill(new Buffer(32), 0),
+    timestamp: 1296688602,
+    bits: 486604799,
+  },
+  genesisBlockTx: module.exports.livenet.genesisBlockTx,
+  proofOfWorkLimit: module.exports.livenet.proofOfWorkLimit,
+  checkpoints: [], // need to put checkput blocks here
+  addressPubkey: 111,
+  addressScript: 196,
+  bip32public: 0x043587cf,
+  bip32private: 0x04358394,
+  privKeyVersion: 239,
+  dnsSeeds: [
+    'testdoge.lionservers.de',
+    'senatorwhiskers.com'
+  ],
+  defaultClientPort: 44556
+
+};
+
+exports.mainnet=exports.bitcoin.mainnet
+exports.livenet=exports.bitcoin.livenet
+exports.testnet=exports.bitcoin.testnet
